ed77329d8256465d7d5f74f76018dba6b2ba2249
Chen_TianHe <yangchen6319@email.com>
2022-09-20 22:05:56 +0800

    初始化

--- main-MKR.py
+++ main-MKR.py
@@ -12,9 +12,9 @@ if __name__ == '__main__':
     # parser.add_argument('--batch_size', type=int, default=1024, help='batch size')
     # parser.add_argument('--epochs', type=int, default=30, help='epochs')
     # parser.add_argument('--device', type=str, default='cuda:0', help='device')
-    # parser.add_argument('--dim', type=int, default=5, help='embedding size')
+    # parser.add_argument('--dim', type=int, default=32, help='embedding size')
     # parser.add_argument('--L', type=int, default=1, help='L')
-    # parser.add_argument('--T', type=int, default=1, help='T')
+    # parser.add_argument('--T', type=int, default=5, help='T')
     # parser.add_argument('--l1', type=float, default=1e-6, help='kg loss weight')
     # parser.add_argument('--ratio', type=float, default=1, help='The proportion of training set used')
 
@@ -24,43 +24,37 @@ if __name__ == '__main__':
     # parser.add_argument('--batch_size', type=int, default=1024, help='batch size')
     # parser.add_argument('--epochs', type=int, default=30, help='epochs')
     # parser.add_argument('--device', type=str, default='cuda:0', help='device')
-    # parser.add_argument('--dim', type=int, default=20, help='embedding size')
+    # parser.add_argument('--dim', type=int, default=32, help='embedding size')
     # parser.add_argument('--L', type=int, default=1, help='L')
-    # parser.add_argument('--T', type=int, default=3, help='T')
+    # parser.add_argument('--T', type=int, default=5, help='T')
     # parser.add_argument('--l1', type=float, default=1e-6, help='kg loss weight')
     # parser.add_argument('--ratio', type=float, default=1, help='The proportion of training set used')
     #
-    # parser.add_argument('--dataset', type=str, default='ml', help='dataset')
-    # parser.add_argument('--lr', type=float, default=1e-2, help='learning rate')
+    parser.add_argument('--dataset', type=str, default='ml', help='dataset')
+    parser.add_argument('--lr', type=float, default=1e-3, help='learning rate')
+    parser.add_argument('--l2', type=float, default=1e-4, help='L2')
+    parser.add_argument('--batch_size', type=int, default=1024, help='batch size')
+    parser.add_argument('--epochs', type=int, default=10, help='epochs')
+    parser.add_argument('--device', type=str, default='cuda:0', help='device')
+    parser.add_argument('--dim', type=int, default=32, help='embedding size')
+    parser.add_argument('--L', type=int, default=1, help='L')
+    parser.add_argument('--T', type=int, default=5, help='T')
+    parser.add_argument('--l1', type=float, default=1e-6, help='kg loss weight')
+    parser.add_argument('--ratio', type=float, default=0.8, help='The proportion of training set used')
+
+    # parser.add_argument('--dataset', type=str, default='yelp', help='dataset')
+    # parser.add_argument('--lr', type=float, default=1e-3, help='learning rate')
     # parser.add_argument('--l2', type=float, default=1e-4, help='L2')
     # parser.add_argument('--batch_size', type=int, default=1024, help='batch size')
     # parser.add_argument('--epochs', type=int, default=30, help='epochs')
     # parser.add_argument('--device', type=str, default='cuda:0', help='device')
-    # parser.add_argument('--dim', type=int, default=10, help='embedding size')
+    # parser.add_argument('--dim', type=int, default=32, help='embedding size')
     # parser.add_argument('--L', type=int, default=1, help='L')
-    # parser.add_argument('--T', type=int, default=1, help='T')
+    # parser.add_argument('--T', type=int, default=5, help='T')
     # parser.add_argument('--l1', type=float, default=1e-6, help='kg loss weight')
     # parser.add_argument('--ratio', type=float, default=1, help='The proportion of training set used')
 
-    parser.add_argument('--dataset', type=str, default='yelp', help='dataset')
-    parser.add_argument('--lr', type=float, default=1e-3, help='learning rate')
-    parser.add_argument('--l2', type=float, default=1e-4, help='L2')
-    parser.add_argument('--batch_size', type=int, default=1024, help='batch size')
-    parser.add_argument('--epochs', type=int, default=30, help='epochs')
-    parser.add_argument('--device', type=str, default='cuda:0', help='device')
-    parser.add_argument('--dim', type=int, default=10, help='embedding size')
-    parser.add_argument('--L', type=int, default=1, help='L')
-    parser.add_argument('--T', type=int, default=1, help='T')
-    parser.add_argument('--l1', type=float, default=1e-6, help='kg loss weight')
-    parser.add_argument('--ratio', type=float, default=1, help='The proportion of training set used')
-
     args = parser.parse_args()
 
-    train(args, True)
+    train(args, False)
 
-'''
-music	train_auc: 0.891 	 train_acc: 0.776 	 eval_auc: 0.823 	 eval_acc: 0.750 	 test_auc: 0.823 	 test_acc: 0.757 		[0.16, 0.28, 0.47, 0.51, 0.51, 0.55, 0.57, 0.58]
-book	train_auc: 0.878 	 train_acc: 0.758 	 eval_auc: 0.751 	 eval_acc: 0.691 	 test_auc: 0.749 	 test_acc: 0.689 		[0.09, 0.15, 0.27, 0.31, 0.31, 0.38, 0.39, 0.42]
-ml	train_auc: 0.934 	 train_acc: 0.835 	 eval_auc: 0.897 	 eval_acc: 0.798 	 test_auc: 0.897 	 test_acc: 0.801 		[0.17, 0.27, 0.51, 0.53, 0.53, 0.62, 0.67, 0.7]
-yelp	train_auc: 0.878 	 train_acc: 0.772 	 eval_auc: 0.832 	 eval_acc: 0.752 	 test_auc: 0.832 	 test_acc: 0.754 		[0.11, 0.17, 0.34, 0.36, 0.36, 0.42, 0.44, 0.49]
-'''
--- src/MKR.py
+++ src/MKR.py
@@ -271,7 +271,7 @@ def get_hrtts(kg_dict, n_item):
 
 
 def train(args, is_topk=False):
-    np.random.seed(555)
+    np.random.seed(123)
     data = load_data(args)
     n_entity, n_user, n_item, n_relation = data[0], data[1], data[2], data[3]
     train_set, eval_set, test_set, rec, kg_dict = data[4], data[5], data[6], data[7], data[8]
@@ -333,8 +333,8 @@ def train(args, is_topk=False):
         eval_auc, eval_acc = eval_ctr(model, eval_set, args.batch_size)
         test_auc, test_acc = eval_ctr(model, test_set, args.batch_size)
 
-        print('epoch: %d \t train_auc: %.3f \t train_acc: %.3f \t '
-              'eval_auc: %.3f \t eval_acc: %.3f \t test_auc: %.3f \t test_acc: %.3f \t' %
+        print('epoch: %d \t train_auc: %.4f \t train_acc: %.4f \t '
+              'eval_auc: %.4f \t eval_acc: %.4f \t test_auc: %.4f \t test_acc: %.4f \t' %
               ((epoch + 1), train_auc, train_acc, eval_auc, eval_acc, test_auc, test_acc), end='\t')
 
         precision_list = []
@@ -355,8 +355,8 @@ def train(args, is_topk=False):
 
     indices = eval_auc_list.index(max(eval_auc_list))
     print(args.dataset, end='\t')
-    print('train_auc: %.3f \t train_acc: %.3f \t eval_auc: %.3f \t eval_acc: %.3f \t '
-          'test_auc: %.3f \t test_acc: %.3f \t' %
+    print('train_auc: %.4f \t train_acc: %.4f \t eval_auc: %.4f \t eval_acc: %.4f \t '
+          'test_auc: %.4f \t test_acc: %.4f \t' %
           (train_auc_list[indices], train_acc_list[indices], eval_auc_list[indices], eval_acc_list[indices],
            test_auc_list[indices], test_acc_list[indices]), end='\t')
 
